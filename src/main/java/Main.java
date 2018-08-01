fa8c1ceb63bc704e809c0fa45de9e8d2df46aba8
Maksim Petrov <smith150384@gmail.com>
2018-08-01 16:59:37 +0300

    Day one

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 
 import ru.smith.dao.RecordDAOImpl;
 import ru.smith.entity.Record;
+import ru.smith.util.LoadDBFromCSV;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -19,22 +21,6 @@ public class Main {
         HibernateUtil.shutdown();
         */
 
-       /* Record record = new Record();
-        record.setSsoid("650ae77a-ffce-475d-a930-c7e345e0658c");
-        record.setTs(1499763594);
-        record.setGrp("guis_-47");
-        record.setType("formcontroller");
-        record.setSubtype("send");
-        record.setUrl("https://www.mos.ru/pgu/ru/application/guis/-47/#step_1");
-        record.setOrgid("guis");
-        record.setFormid("-47");
-        record.setCode("MPGU");
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH");
-        LocalDateTime dateTime = LocalDateTime.parse("2017-07-11-09", formatter);
-        record.setYmdh(dateTime);
-        new RecordDAOImpl().saveOrUpdate(record);
-*/
-
-
+        LoadDBFromCSV.load("./src/main/resources/test_case.csv");
     }
 }
--- src/main/java/ru/smith/util/LoadDBFromCSV.java
+++ src/main/java/ru/smith/util/LoadDBFromCSV.java
@@ -1,4 +1,72 @@
 package ru.smith.util;
 
+import ru.smith.dao.RecordDAOImpl;
+import ru.smith.entity.Record;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class LoadDBFromCSV {
+
+    public static void load(String path) {
+        Record record;
+        RecordDAOImpl recordDAOImpl = new RecordDAOImpl();
+
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+
+        try {
+            br = new BufferedReader(new FileReader(path));
+
+            line = br.readLine();
+            line = br.readLine();
+
+
+           // while ((line = br.readLine()) != null) {
+
+                String[] row = line.split(cvsSplitBy);
+
+                if (row.length == 12) {
+
+                    record = new Record();
+                    record.setSsoid(row[0]);
+                    record.setTs(Instant.ofEpochSecond(Long.parseLong(row[1])));
+                    record.setGrp(row[2]);
+                    record.setType(row[3]);
+                    record.setSubtype(row[4]);
+                    record.setUrl(row[5]);
+                    record.setOrgid(row[6]);
+                    record.setFormid(row[7]);
+                    record.setCode(row[8]);
+                    record.setLtpa(row[9]);
+                    record.setSudirresponse(row[10]);
+                    record.setYmdh(LocalDateTime.parse(row[11], DateTimeFormatter.ofPattern("yyyy-MM-dd-HH")));
+
+
+                    recordDAOImpl.saveOrUpdate(record);
+                    recordDAOImpl.saveOrUpdate(record);
+                    recordDAOImpl.saveOrUpdate(record);
+                }
+            //}
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
