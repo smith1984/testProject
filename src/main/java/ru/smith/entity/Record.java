337a3d8fb15f7e199170a2e58584d81b05c69bc2
Maksim Petrov <smith150384@gmail.com>
2018-08-03 19:55:23 +0300

    Day three

--- src/main/java/ru/smith/entity/Record.java
+++ src/main/java/ru/smith/entity/Record.java
@@ -19,13 +19,25 @@ import java.time.LocalDateTime;
         ),
         @NamedQuery(
                 name = "ratingForms",
-                query = "SELECT COUNT(r.formid), r.formid, r.orgid FROM Record r GROUP BY r.orgid, r.formid ORDER BY COUNT(r.formid) DESC"
+                query = "SELECT COUNT(r.formid), r.formid, r.orgid FROM Record r" +
+                        " GROUP BY r.orgid, r.formid ORDER BY COUNT(r.formid) DESC"
         ),
         @NamedQuery(
                 name = "countUseForm",
                 query = "SELECT COUNT(r.formid) FROM Record r"
         )
 })
+@NamedNativeQuery(
+        name = "stopUseForm",
+        query = "select r.ts, r.ssoid, r.formid, r.type, r.subtype from records r " +
+                "join (select max(ts) ts, ssoid, formid from records group by " +
+                "ssoid, formid) AS rec on r.ts = rec.ts and r.ssoid = rec.ssoid" +
+                " and r.formid = rec.formid where r.formid is not null" +
+                " and r.ssoid <> '' and r.ssoid <> 'Unauthorized' " +
+                "and r.subtype <> '' and subtype" +
+                " not in ('send', 'sent', 'done', 'success') order by r.ts"
+)
+
 
 @Entity
 @Table(name = "records")
